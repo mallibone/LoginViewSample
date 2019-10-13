81afff843f5ad7357351ea79e1e990ce9ed3e6c7
Mark Allibone <m.allibone@rey-automation.ch>
2019-10-13 20:44:52 +0200

    Fix image not found error during app start on android

--- LoginViewSample/LoginViewSample.Android/MainActivity.cs
+++ LoginViewSample/LoginViewSample.Android/MainActivity.cs
@@ -1,15 +1,11 @@
-﻿using System;
-
+﻿
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace LoginViewSample.Droid
 {
-	[Activity (Label = "LoginViewSample", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity (Label = "LoginViewSample", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate (Bundle bundle)
